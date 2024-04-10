cae199cfb8c4d15b0e85dc79c4194bf9a9af4d37
luke-wyeth <luke.wyeth@mail.com>
2024-04-10 21:52:12 +1200

    test adding new route and deploy to vercel

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 8085;
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/test", (req, res) => res.json({hello: 'hello world!'}));
 
-app.listen(port, () => console.log("Server ready on port " + port));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
 
 
-// C:\Users\Luke\AppData\Roaming\npm\vercel.ps1 dev  
+// C:\Users\Luke\AppData\Roaming\npm\vercel.ps1
