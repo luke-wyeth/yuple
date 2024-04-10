0fa98ca3df2491ec3a2bcc98c3159996d13ba9bb
luke-wyeth <luke.wyeth@mail.com>
2024-04-10 20:45:52 +1200

    move to monorepo structure

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory, createWebHashHistory  } from 'vue-route
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
