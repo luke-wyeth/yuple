233870bcb966e2189f8afda33dd1df7de982aaab
luke-wyeth <luke.wyeth@mail.com>
2025-03-06 19:33:13 +1300

    get tailwind + font awesome icons working

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,28 @@
+<template>
+  <div class='h-full'>
+    <header>
+
+        <nav class="w-full bg-linear-65 from-orange to-orange-pink shadow-lg h-14 md:h-18 xl:h-20 mb-4">
+          <RouterLink to="/"><font-awesome-icon :icon="['far', 'user']" /></RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/login">Login</RouterLink>
+        </nav>
+
+    </header>
+
+    <RouterView />
+
+    <footer>
+      <nav class="fixed bottom-0 w-full bg-linear-65 from-orange-pink to-pink-purple shadow-xl h-16 md:h-20 xl:h-22">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+      </nav>
+    </footer>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 
 </script>
-
-<template>
-  <div>
-    <header>
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-    </header>
-
-    <RouterView />
-  </div>
-</template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,25 @@
-// import './assets/main.css'
+import './assets/main.css'; // Import the CSS file
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(far)
+
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 
 app.mount('#app')
