9499f5a1014647e4d541a16d481e42c0ce7ccd99
luke-wyeth <luke.wyeth@mail.com>
2025-03-06 21:23:35 +1300

    keep working on basic card

---
+++ frontend/src/components/BasicCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="relative">
+    <div
+      :class="{
+        'blur absolute inset-0 rounded-md bg-gradient-to-br from-orange via-orange-pink via-pink-purple via-purple via-pink-purple via-orange-pink to-orange':
+          glowingBorder,
+      }"
+    />
+
+    <div
+      class="relative rounded-md bg-gray-100 ring-1 ring-gray-900/5"
+      :class="{
+        'shadow-lg': !glowingBorder,
+      }"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps({
+  glowingBorder: Boolean,
+});
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <main>
     Home
+
+    <basic-card :glowing-border="true" class="mb-12"> <div class="h-24">this is a glowing card</div> </basic-card>
+    <basic-card :glowing-border="false"> <div class="h-24">this is a basic card</div> </basic-card>
   </main>
 </template>
+
+<script setup lang="ts">
+import BasicCard from '../components/BasicCard.vue'
+</script>
