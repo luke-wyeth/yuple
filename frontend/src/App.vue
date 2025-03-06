6897a244bb9bcb79c4295aa6ff2b9592277fb711
luke-wyeth <luke.wyeth@mail.com>
2025-03-06 20:54:59 +1300

    always take min screen height

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class='h-full'>
+  <div class='h-full min-h-screen flex flex-col'>
     <header>
 
-        <nav class="w-full p-2 bg-linear-65 from-orange to-orange-pink shadow-lg h-14 md:h-18 xl:h-20 mb-4 flex flex-row justify-between items-center text-white">
+        <nav class="w-full bg-linear-65 from-orange to-orange-pink shadow-lg h-14 md:h-18 xl:h-20 p-4 flex flex-row justify-between items-center text-white">
           <div class="flex gap-2">
             <RouterLink to="/">Home</RouterLink>
             <RouterLink to="/about">About</RouterLink>
@@ -12,11 +12,11 @@
 
     </header>
 
-    <RouterView />
+    <RouterView class="p-4 flex-grow"/>
 
-    <nav class="sticky bottom-0 w-full bg-linear-65 from-orange-pink to-pink-purple shadow-xl h-16 md:h-20 xl:h-22"> </nav>
+    <nav class="sticky bottom-0 w-full bg-linear-65 from-orange-pink to-pink-purple shadow-xl p-4 h-16 md:h-20 xl:h-22"> </nav>
     <footer>
-      <div class="bg-linear-65 from-purple to-navy h-24"> </div>
+      <div class="bg-linear-65 from-purple to-navy p-4 h-24"> </div>
     </footer>
   </div>
 </template>
